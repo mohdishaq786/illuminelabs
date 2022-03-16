067f0332c9d020510ce707d7850c6d9662fb3d61
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-03-16 12:52:49 +0530

    Add files via upload

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,30 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import firebase from './fireconf';
 import Todo from './Todo';
+import { Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 function TodoList() {
   const [todoList, setTodoList] = useState();
-
+  const [mtodoList, setMtodoList] = useState();
+  const [search,setSearch]=useState();
+//similar to componentdidmount and componentdid update 
+//but cpmponentunmount?? ka kya?
   useEffect(() => {
     const todoRef = firebase.database().ref('Todo');
     todoRef.on('value', (snapshot) => {
       const todos = snapshot.val();
       const todoList = [];
       for (let id in todos) {
-        todoList.push({ id, ...todos[id] });
+        todoList.push({ id, ...todos[id] }); ///current me jo hai
      
-
+        
       }
+        
       setTodoList(todoList);
+      setMtodoList(todoList);
     });
   }, []);
+  const SearchItem =(e)=>{
+   e.preventDefault();   //cancels the event if it is cancelable, ??
+    // setTodoList(todoList.filter((i)=>
+    //    todoList.toLowerCase().includes(search.toLowerCase())
+    // ));
+
+    {/*in my method only one time we search in future use saket sir method */}
+    // FilterAction.searchByKey = (list, key, value) => {
+
+
+
+      // saket sir method
+      let text = search
+        .trim()
+        .toLowerCase()
+        .replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      let fval=[]
+      if (!text || text === "") {
+        // fval=mtodoList;
+        alert("Enter todo to search");
+         return false;
+
+      } 
+      else {
+        debugger
+        fval=mtodoList.filter((item) => {
+          return item.title.toLowerCase().match(text);
+        });
+      }
+      debugger
+      setTodoList(fval);
+      
+       
+    // };
+  };
+  
 
   return (
-    <div>
-      {todoList
-        ? todoList.map((todo, index) => <Todo todo={todo} key={index} />)
-        : ''}
-    </div>
+    <>
+      <form onSubmit={(e)=>{SearchItem(e)}}>  
+         <input placeholder='Search....' onChange={(e)=>{setSearch(e.target.value)}}/>
+         <button type="submit"><SearchOutlined /></button>
+       </form>
+      {todoList ? todoList.map((todo, index) => <Todo todo={todo} key={index} />) 
+        : ''}   
+        {/* kuch nai hm bus ternary opertor if this then this */}
+    </>
   );
 }
 
--- src/components/form.js
+++ src/components/form.js
@@ -3,32 +3,68 @@ import React, { useState } from 'react';
 //import { database } from '../firebase';
 import firebase from './fireconf';
 import { EditOutlined } from '@ant-design/icons';
+//import CreateTodo from './createtodo';
+
 const Form = () => {
   
 
-    const [addkaro, setAddkaro] = useState('');
+    const [title, setTitle] = useState('');
+    const [search,setSearch]=useState("");
 
     const handleOnChange = (e) => {
-      setAddkaro(e.target.value); //?MEANS BUT MERA HISAB SE YE TARGET VALUE KO ACCESS KR RHE HAI
-    };
-    const createTodo = () => {
-  //    const todoRef =ref(database,'Todo'); 
-      const todoRef = firebase.database().ref('Todo');//CORECT METHOD
-      const todo = {
-        addkaro,
-        complete: false,
-      };
   
-      todoRef.push(todo);  //INSERT PUSH SAME AS NORMAL VECTOR IT 
+     
+    
+      setTitle(e.target.value); //?MEANS BUT MERA HISAB SE YE TARGET VALUE KO ACCESS KR RHE HAI
+
+
     };
+    
+    const createTodo = () => {
+  //    const todoRef =ref(database,'Todo'); //new version method
+       
+      const todoRef = firebase.database().ref('Todo');//CORECT METHOD live project we use this
+      const todo = {
+       
+        title,
+        complete: false,
+        
+      };
+      if(todo.title==="")
+       {
+         //console.log(todo.title)
+         alert("Enter your task");
+         return false;
+       }
+       else
+       {
+       todoRef.push(todo);
+        setTitle('');
+       }
+        //INSERT PUSH SAME AS hai but isme  push auto unique key generate krti hai
+    };
+    
+  // const SearchItem =(e)=>{
+  //   e.preventDefault();   //cancels the event if it is cancelable, ??
+  //   setTitle(title.filter((i)=>
+  //      i.title.tolowerCase().includes(search.toLocaleLowerCase())
+  //   ));
+  // };
 
-
+ 
 
 
   return (
     <div>
-       <input type="text" onChange={handleOnChange} value={addkaro} />
-      <button onClick={createTodo}><EditOutlined style={{color:"#66788a"}}/></button>
+      
+       <input placeholder ="Enter your Task" type ="text" onChange={handleOnChange} value={title} />{'\n'}
+       <button onClick={createTodo}><EditOutlined style={{color:"#66788a"}}/></button>
+       
+       <br/>
+       
+          
+      
+      
     </div>
   );
 }
